9938ace080f89ac91a932e729a65087677d5201b
gijs.sijpesteijn <gijs@jpoint.nl>
2017-05-08 11:02:18 +0200

    test: Test coverage

---
+++ bundles/ngx-delete-confirm.umd.js
@@ -0,0 +1,280 @@
+/**
+ * ngx-delete-confirm - 
+ * @version v0.0.2
+ * @author gijs.sijpesteijn
+ * @link https://github.com/sijpesteijn/ngx-delete-confirm#readme
+ * @license MIT
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("@angular/core"));
+	else if(typeof define === 'function' && define.amd)
+		define(["@angular/core"], factory);
+	else if(typeof exports === 'object')
+		exports["ngxClickToEdit"] = factory(require("@angular/core"));
+	else
+		root["ngxClickToEdit"] = factory(root["ng"]["core"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_6__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = __webpack_require__(6);
+var NgxDeleteConfirmComponent = (function () {
+    function NgxDeleteConfirmComponent() {
+        this.question = 'Are you sure?';
+        this.onConfirm = new core_1.EventEmitter();
+        this.onCancel = new core_1.EventEmitter();
+        this.show = false;
+    }
+    NgxDeleteConfirmComponent.prototype.sendConfirm = function () {
+        this.onConfirm.emit();
+        this.show = false;
+    };
+    NgxDeleteConfirmComponent.prototype.sendCancel = function () {
+        this.onCancel.emit();
+        this.show = false;
+    };
+    return NgxDeleteConfirmComponent;
+}());
+__decorate([
+    core_1.Input('question'),
+    __metadata("design:type", String)
+], NgxDeleteConfirmComponent.prototype, "question", void 0);
+__decorate([
+    core_1.Output('onConfirm'),
+    __metadata("design:type", core_1.EventEmitter)
+], NgxDeleteConfirmComponent.prototype, "onConfirm", void 0);
+__decorate([
+    core_1.Output('onCancel'),
+    __metadata("design:type", core_1.EventEmitter)
+], NgxDeleteConfirmComponent.prototype, "onCancel", void 0);
+NgxDeleteConfirmComponent = __decorate([
+    core_1.Component({
+        selector: 'ngx-delete-confirm',
+        template: __webpack_require__(4),
+        styles: [__webpack_require__(5)]
+    })
+], NgxDeleteConfirmComponent);
+exports.NgxDeleteConfirmComponent = NgxDeleteConfirmComponent;
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(__webpack_require__(0));
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(3)(undefined);
+// imports
+
+
+// module
+exports.push([module.i, ".delete-confirm {\n    background-color: transparent;\n}\n\n.delete-confirm-btn {\n}\n\n.delete-confirm-question {\n    color: white;\n    background-color: red;\n    float: left;\n    position: absolute;\n    z-index: 1;\n    padding: 5px 5px 5px 8px;\n    border-radius: 7px;\n    margin: -5px;\n}\n\n.delete-confirm-question button {\n    background-color: red;\n}\n\n.delete-confirm-question i {\n    color: white;\n}\n\n.fa-times, fa-check {\n    color: white;\n}", ""]);
+
+// exports
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+module.exports = function(useSourceMap) {
+	var list = [];
+
+	// return the list of modules as css string
+	list.toString = function toString() {
+		return this.map(function (item) {
+			var content = cssWithMappingToString(item, useSourceMap);
+			if(item[2]) {
+				return "@media " + item[2] + "{" + content + "}";
+			} else {
+				return content;
+			}
+		}).join("");
+	};
+
+	// import a list of modules into the list
+	list.i = function(modules, mediaQuery) {
+		if(typeof modules === "string")
+			modules = [[null, modules, ""]];
+		var alreadyImportedModules = {};
+		for(var i = 0; i < this.length; i++) {
+			var id = this[i][0];
+			if(typeof id === "number")
+				alreadyImportedModules[id] = true;
+		}
+		for(i = 0; i < modules.length; i++) {
+			var item = modules[i];
+			// skip already imported module
+			// this implementation is not 100% perfect for weird media query combinations
+			//  when a module is imported multiple times with different media queries.
+			//  I hope this will never occur (Hey this way we have smaller bundles)
+			if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+				if(mediaQuery && !item[2]) {
+					item[2] = mediaQuery;
+				} else if(mediaQuery) {
+					item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+				}
+				list.push(item);
+			}
+		}
+	};
+	return list;
+};
+
+function cssWithMappingToString(item, useSourceMap) {
+	var content = item[1] || '';
+	var cssMapping = item[3];
+	if (!cssMapping) {
+		return content;
+	}
+
+	if (useSourceMap && typeof btoa === 'function') {
+		var sourceMapping = toComment(cssMapping);
+		var sourceURLs = cssMapping.sources.map(function (source) {
+			return '/*# sourceURL=' + cssMapping.sourceRoot + source + ' */'
+		});
+
+		return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
+	}
+
+	return [content].join('\n');
+}
+
+// Adapted from convert-source-map (MIT)
+function toComment(sourceMap) {
+	// eslint-disable-next-line no-undef
+	var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
+	var data = 'sourceMappingURL=data:application/json;charset=utf-8;base64,' + base64;
+
+	return '/*# ' + data + ' */';
+}
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"delete-confirm\">\n    <a href=\"javascript:void(0)\" class=\"delete-confirm-btn\" (click)=\"show = true\">\n        <div #ref><ng-content></ng-content></div>\n        <i class=\"fa fa-trash\" *ngIf=\"ref.childNodes.length == 0\"></i>\n    </a>\n    <div class=\"delete-confirm-question\" *ngIf=\"show === true\">\n        <button class='btn-x-sm' (click)='sendCancel()'><i class=\"fa fa-times\"></i></button>\n        <button class='btn-x-sm' (click)='sendConfirm()'><i class=\"fa fa-check\"></i></button>\n        <span [innerHTML]=\"question\"></span>\n    </div>\n</div>";
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+        var result = __webpack_require__(2);
+
+        if (typeof result === "string") {
+            module.exports = result;
+        } else {
+            module.exports = result.toString();
+        }
+    
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_6__;
+
+/***/ })
+/******/ ]);
+});
+//# sourceMappingURL=ngx-delete-confirm.umd.js.map
